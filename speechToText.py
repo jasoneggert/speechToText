e00ee3ad9234692d73352febbceb62aa99e5f4db
gert <jeggert@jeggert.com>
2023-04-11 12:55:53 -0400

    adding provided summary via gtp3.5turbo

--- speechToText.py
+++ speechToText.py
@@ -7,7 +7,8 @@ import openai
 load_dotenv()
 
 # Parse command-line arguments
-parser = argparse.ArgumentParser(description='Transcribe audio and generate summary using OpenAI Whisper and GPT-3')
+parser = argparse.ArgumentParser(
+    description='Transcribe audio and generate summary using OpenAI Whisper and GPT-3')
 parser.add_argument('file_path', type=str, help='Path to the audio file')
 args = parser.parse_args()
 
@@ -20,11 +21,14 @@ audio_file_path = args.file_path
 with open(audio_file_path, "rb") as audio_file:
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     print("Transcript:\n", transcript)
-    
+
     # Generate summary using GPT-3
-    summary = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Generate a summary of the key points from the audio transcription:\n{transcript}",
+    summary = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You help condense transcriptions of meetings into a brief summary of key points."},
+            {"role": "user", "content": f"Generate a summary of the key points from the audio transcription:\n{transcript}"}
+        ],  # Added a comma here
         max_tokens=1000
     )
-    print("Summary:\n", summary.choices[0].text)
+    print("Summary:\n", summary)
