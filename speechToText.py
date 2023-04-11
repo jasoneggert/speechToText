1ca9c6a5709f79575d82d84a5d26599c8dd7046b
gert <jeggert@jeggert.com>
2023-04-11 12:23:34 -0400

    provide generative summary of the audio transcription

--- speechToText.py
+++ speechToText.py
@@ -7,7 +7,7 @@ import openai
 load_dotenv()
 
 # Parse command-line arguments
-parser = argparse.ArgumentParser(description='Transcribe audio using OpenAI Whisper API')
+parser = argparse.ArgumentParser(description='Transcribe audio and generate summary using OpenAI Whisper and GPT-3')
 parser.add_argument('file_path', type=str, help='Path to the audio file')
 args = parser.parse_args()
 
@@ -19,4 +19,12 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 audio_file_path = args.file_path
 with open(audio_file_path, "rb") as audio_file:
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    print(transcript)
+    print("Transcript:\n", transcript)
+    
+    # Generate summary using GPT-3
+    summary = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"Generate a summary of the key points from the audio transcription:\n{transcript}",
+        max_tokens=1000
+    )
+    print("Summary:\n", summary.choices[0].text)
